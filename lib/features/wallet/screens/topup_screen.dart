fe0352f2f0405d5fccdde103c5db75f6068cbce3
Evand <alfeushendrievan@gmail.com>
2026-06-23 19:38:00 +0700

    bikin topup page

--- lib/features/wallet/screens/topup_screen.dart
+++ lib/features/wallet/screens/topup_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class TopUpScreen extends StatelessWidget {
+class TopUpScreen extends StatefulWidget {
   const TopUpScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Top Up Screen")));
+  State<TopUpScreen> createState() => _TopUpScreenState();
+}
+
+class _TopUpScreenState extends State<TopUpScreen> {
+  final TextEditingController amountController = TextEditingController();
+
+  bool loading = false;
+  int selectedAmount = 0;
+
+  final List<int> quickAmounts = [10000, 20000, 50000, 100000, 200000, 500000];
+
+  Future<int> getBalance() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    final doc = await FirebaseFirestore.instance
+        .collection('wallets')
+        .doc(uid)
+        .get();
+
+    return (doc.data()?['balance'] ?? 0) as int;
   }
 }
