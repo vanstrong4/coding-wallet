79c012c0816f5aa5c6f9873b0c0948c95bf30acb
Evand <alfeushendrievan@gmail.com>
2026-06-23 20:19:54 +0700

    bikin history

--- lib/features/wallet/screens/history_screen.dart
+++ lib/features/wallet/screens/history_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({super.key});
 
+  Color _getColor(String type) {
+    switch (type) {
+      case 'topup':
+        return Colors.green;
+      case 'purchase':
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  IconData _getIcon(String type) {
+    switch (type) {
+      case 'topup':
+        return Icons.add_circle;
+      case 'purchase':
+        return Icons.shopping_cart;
+      default:
+        return Icons.receipt_long;
+    }
+  }
+
+  String _getTitle(String type) {
+    switch (type) {
+      case 'topup':
+        return "Top Up";
+      case 'purchase':
+        return "Pembelian";
+      default:
+        return "Transaksi";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("History Screen")));
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    return Scaffold(backgroundColor: const Color(0xFFF5F7FB));
   }
 }
